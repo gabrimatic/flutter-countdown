af98002d3a5307f3929310b71aa347570346891b
Hossein Yousefpour <h.yusefpour@gmail.com>
2022-05-22 19:37:38 +0430

    - Migrated to null safety - Converted to be a package - Updated to Flutter 2.17

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:countdown_flutter/countdown_flutter.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
--- ios/Classes/CountdownFlutterPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface CountdownFlutterPlugin : NSObject<FlutterPlugin>
-@end
